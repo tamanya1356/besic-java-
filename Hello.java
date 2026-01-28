c8c87970c39eb91bf7e451f5244c6b80ebffda29
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-28 13:53:41 +0530

    adding 1st java program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+ class Hello {
+    public  static void main (String args[]){
+ System.out.println("Hello world!"); // println is use for go to next line 
+System.out.println("Hello world!"); // print is use for write same line 
+System.out.println("Hello world!");
+System.out.println("Hello world!\nfrom\nTamanya\nand\nSoumalya");
+    } // \n is used for write the sentences in next to next line
+ }
