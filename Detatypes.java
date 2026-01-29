d1da7e990898fb477a465a857df1274f06005268
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-29 10:33:14 +0530

    5th java code

---
+++ Detatypes.java
@@ -0,0 +1,10 @@
+public class Detatypes {
+    public static void main(String args[]) {
+ int a=70;
+ int b=50;
+ int sum=a+b;
+System.out.println(sum);
+int mul=a*b;
+System.out.println(mul);
+}
+}
