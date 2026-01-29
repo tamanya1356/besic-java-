644752d191c78342b5b884401340b9f145a75819
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-29 10:31:33 +0530

    3rd java code

---
+++ ConditionIfelseelse.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ConditionIfelseelse {
+  public static void main (String args[]){
+
+  
+  Scanner sc=new Scanner(System.in);{
+    int a=sc.nextInt(); // take a and b as an input 
+    int b=sc.nextInt();
+  if(a==b){
+    System.out.println("Equal"); 
+  } else if (a>b){
+    System.out.println("a is greater");
+  }else{
+    System.out.println("b is greater");
+  }
+
+}
+
+
+
+    }
+
+
+
+
+
+  }  
+
