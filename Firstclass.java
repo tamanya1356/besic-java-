6c346f0c890c38bcb7d50477a15a2dd0c9f1158f
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-31 11:50:07 +0530

    7th code

---
+++ Firstclass.java
@@ -0,0 +1,22 @@
+// class Firstclass {
+// public static void main(String args[]) { // capital s in string 
+
+// System.out.println("Hello world!"); // capital s for system 
+// }
+
+// }
+
+// class Firstclass{
+
+//     public static void main (String args[])
+// {
+//     System.out.println("Hello world!");
+// }
+
+
+// }
+class Firstclass{
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+}
