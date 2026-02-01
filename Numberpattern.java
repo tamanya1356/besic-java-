fdff275b44a16e58f76138f82f88c769bf2d30ac
Tamanya <tamanyaghoshal7@gmail.com>
2026-02-01 12:25:21 +0530

    11 th code

---
+++ Numberpattern.java
@@ -0,0 +1,20 @@
+public class Numberpattern {
+    public static void main(String args[]){
+   int n=5;
+   //outer loop 
+   for(int i=1;i<=n;i++){
+ // inner loop 
+ for(int j=1;j<=i;j++){
+
+    System.out.print(j+""); // when i=1,j=1 to i obdhi print hobbe 
+ }
+System.out.println();
+
+   }
+
+
+
+
+    }
+    
+}
